b25796293f09e4c3cbc5fe31844aa510c92f8cff
Varun Seth <8343593+varun-seth@users.noreply.github.com>
2024-02-18 14:21:04 +0530

    Choice View shows radio buttons and checkbox
    - Easily distinguish between single and multiple choice questions

--- src/registerIcons.ts
+++ src/registerIcons.ts
@@ -3,11 +3,20 @@ import {
     faArrowLeft,
     faArrowRight,
     faRedo,
-    faCheckDouble,
+	faCheckDouble,
     faCog,
     faCircleNotch,
 	faLightbulb,
+	faCheckSquare,
+	faSquare,
+	faDotCircle,
 } from '@fortawesome/free-solid-svg-icons';
+
+import {
+	faSquare as faSquareRegular, 
+	faCircle as faCircleRegular,
+	faDotCircle as faDotCircleRegular
+} from '@fortawesome/free-regular-svg-icons';
 
 export default function () {
     config.autoAddCss = false;
@@ -16,7 +25,13 @@ export default function () {
         faArrowRight,
         faRedo,
         faLightbulb,
-        faCheckDouble,
-        faCircleNotch
+		faCheckDouble,
+        faCircleNotch,
+		faCheckSquare,
+		faSquare,
+		faSquareRegular,
+		faCircleRegular,
+		faDotCircle,
+		faDotCircleRegular,
     );
 }
