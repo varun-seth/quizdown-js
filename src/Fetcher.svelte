248088a1cfb484c765fb23ba8b50cb44836411a9
Varun Seth <8343593+varun-seth@users.noreply.github.com>
2024-03-09 11:45:49 +0530

    gdrive fetches user info

--- src/Fetcher.svelte
+++ src/Fetcher.svelte
@@ -1,19 +1,47 @@
 <script>
     // This will be replaced at build time with the actual value
     const apiKey = process.env.API_KEY;
-
     export function gdriveFetch(fileId, callbackFn) {
-        const url = `https://www.googleapis.com/drive/v3/files/${fileId}?alt=media&key=${apiKey}`;
+        const metadataUrl = `https://www.googleapis.com/drive/v3/files/${fileId}?fields=owners&key=${apiKey}`;
+        const contentUrl = `https://www.googleapis.com/drive/v3/files/${fileId}?alt=media&key=${apiKey}`;
 
-        fetch(url)
+        // Fetch file metadata
+        fetch(metadataUrl)
             .then((response) => {
                 if (!response.ok) {
-                    throw new Error('Network response was not ok');
+                    throw new Error('Failed to fetch metadata');
                 }
-                return response.text();
+                return response.json();
             })
-            .then((text) => {
-                callbackFn(text);
+            .then((metadata) => {
+                // Extract parameters from metadata
+                const params = {};
+                if (metadata.owners && metadata.owners.length > 0) {
+                    if (metadata.owners.length > 1) {
+                        console.warn(
+                            'We only support a single owner currently'
+                        );
+                    }
+                    const owner = metadata.owners[0];
+                    params.authorName = owner.displayName;
+                    // Note: photoLink might not always be present
+                    params.authorImageUrl =
+                        owner.photoLink || 'defaultImageUrl';
+                    params.authorUrl = 'mailto://' + owner.emailAddress;
+                }
+
+                // Fetch file content
+                fetch(contentUrl)
+                    .then((response) => {
+                        if (!response.ok) {
+                            throw new Error('Failed to fetch file content');
+                        }
+                        return response.text();
+                    })
+                    .then((text) => {
+                        // Call callback with content and parameters
+                        callbackFn(text, params);
+                    });
             })
             .catch((error) => console.error('Error:', error));
     }
